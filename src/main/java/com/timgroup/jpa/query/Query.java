9ab858c6a2f2e864a4de17baf144f7ba3648c00d
Tom Anderson <tom.anderson@timgroup.com>
2012-07-18 16:56:30 +0100

    Made query runner a bit more funky

--- src/main/java/com/timgroup/jpa/query/Query.java
+++ src/main/java/com/timgroup/jpa/query/Query.java
@@ -8,8 +8,23 @@ import com.timgroup.jpa.Idea;
 public abstract class Query extends Statement {
     
     @Override
-    public void execute(EntityManager em) {
-        dumpQueryResults(findIdeasOnAParticularStock(em));
+    public void execute(final EntityManager em) {
+        attempt("findIdeasOnAParticularStock", new Runnable() {
+            @Override public void run() {
+                dumpQueryResults(findIdeasOnAParticularStock(em));
+            }
+        });
+    }
+    
+    private void attempt(String name, Runnable feat) {
+        System.out.println(name);
+        System.out.println("===========================");
+        try {
+            feat.run();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public abstract TypedQuery<Idea> findIdeasOnAParticularStock(EntityManager em);
