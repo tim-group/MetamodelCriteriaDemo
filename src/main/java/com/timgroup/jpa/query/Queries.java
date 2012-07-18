445ac8d901806c20f19fbd9d6ef48b94434b10ff
Tom Anderson <tom.anderson@timgroup.com>
2012-07-18 16:35:25 +0100

    Adding Queries class i didn't check in earlier (i suck SO MUCH)

---
+++ src/main/java/com/timgroup/jpa/query/Queries.java
@@ -0,0 +1,18 @@
+package com.timgroup.jpa.query;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.timgroup.jpa.Idea;
+
+public abstract class Queries extends Statement {
+    
+    @Override
+    public void execute(EntityManager em) {
+        TypedQuery<Idea> query = query(em);
+        dumpQueryResults(query);
+    }
+    
+    public abstract TypedQuery<Idea> query(EntityManager em);
+    
+}
