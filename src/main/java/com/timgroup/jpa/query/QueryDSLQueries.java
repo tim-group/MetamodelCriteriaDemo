48c992465aa6afc3a35a77c501ab64564f40bcf7
Tom Anderson <tom.anderson@timgroup.com>
2012-07-19 11:39:16 +0100

    Cleaned up QueryDSLQueries

--- src/main/java/com/timgroup/jpa/query/QueryDSLQueries.java
+++ src/main/java/com/timgroup/jpa/query/QueryDSLQueries.java
@@ -6,30 +6,33 @@ import javax.persistence.TypedQuery;
 import com.mysema.query.jpa.impl.JPAQuery;
 import com.timgroup.jpa.Author;
 import com.timgroup.jpa.Idea;
-import com.timgroup.jpa.QAuthor;
-import com.timgroup.jpa.QIdea;
+
+import static com.timgroup.jpa.QAuthor.author;
+import static com.timgroup.jpa.QIdea.idea;
 
 public class QueryDSLQueries extends Query {
     
     @Override
     public TypedQuery<Idea> findIdeasOnAParticularStock(EntityManager em) {
-        QIdea idea = QIdea.idea;
+        
         JPAQuery dslQuery = new JPAQuery(em).from(idea).where(idea.stockTicker.eq("NXJ"));
+        
         return (TypedQuery<Idea>) dslQuery.createQuery(idea);
     }
 
     @Override
     public TypedQuery<Idea> findIdeasWithBigInvestments(EntityManager em) {
-        QIdea idea = QIdea.idea;
+        
         JPAQuery dslQuery = new JPAQuery(em).from(idea).where(idea.investment.gt(1500000));
+        
         return (TypedQuery<Idea>) dslQuery.createQuery(idea);
     }
     
     @Override
     public TypedQuery<Author> findAuthorsOfIdeasOnAParticularStock(EntityManager em) {
-        QAuthor author = QAuthor.author;
-        QIdea idea = QIdea.idea;
+        
         JPAQuery dslQuery = new JPAQuery(em).from(author).join(author.ideas, idea).where(idea.stockTicker.eq("NXJ"));
+        
         return (TypedQuery<Author>) dslQuery.createQuery(author);
     }
     
